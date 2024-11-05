07108d8b2d10b99eade8f9336ac16511c46ec322
braeson-nyahera <braebulimo@gmail.com>
2024-11-05 19:49:18 +0300

    added post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    Content =models.TextField()
+    author =models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at =models.DateTimeField(default=timezone.now)
+    updated_at =models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
