45095ad2143823fd9de1ccd1caa01a4f25f0dd04
braeson-nyahera <braebulimo@gmail.com>
2024-11-12 15:41:40 +0300

    added forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
 
 urlpatterns = [
     path('',PostListView.as_view(),name='home'),
     path('detail/<int:pk>/',PostDetailView.as_view(),name='post-detail'),
+    path('add/',PostCreateView ,name='add-post'),
+    path('edit/<int:id>/',PostUpdateView,name='edit-post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
 from django.views.generic import ListView,DetailView
+from .forms import PostForm
 
 # Create your views here.
 class PostListView(ListView):
     model = Post
     template_name = "blog/home.html"
     context_object_name = "posts"
+    ordering = ['-created_at']
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post-detail.html"
+
+def PostCreateView(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            new_post = post_form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
+
+            return redirect('home')
+    else:
+        post_form = PostForm()
+
+    context = {
+        'post_form' : post_form,
+    }
+    return render(request,'blog/add-post.html', context)
+
+def PostUpdateView(request, id):
+    post = get_object_or_404(Post,id=id)
+    if request.method =='POST':
+        post_edit = PostForm(request.POST, instance = post)
+
+        if post_edit.is_valid():
+            post_edit.save()
+
+            return redirect('post-detail', post.id)
+        
+    else:
+        post_edit = PostForm(instance=post)
+
+    context = {
+        'post_edit':post_edit
+    }
+
+    return render(request, 'blog/edit-post.html', context)
