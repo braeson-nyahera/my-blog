ade1a25d3dbd34f844a249025bb30093e65deeea
braeson-nyahera <braebulimo@gmail.com>
2024-11-13 22:08:16 +0300

    added comments

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,related_name= 'comments' , on_delete= models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
