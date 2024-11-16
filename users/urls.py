aef4c1acf6af1554f9ca34dff4e7071e92926a0b
braeson-nyahera <braebulimo@gmail.com>
2024-11-16 16:09:28 +0300

    register form

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import logout_confirm, logout_done
+from .views import logout_confirm, logout_done, register
 
 
 urlpatterns = [
     path("login/",auth_views.LoginView.as_view(template_name = 'users/login.html'), name= 'login'),
     path("logout/",logout_confirm, name = 'logout-confirm' ),
     path("logout-done/", logout_done, name='logout-done'),
+    path("register/", register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import logout
-
+from .forms import UserRegisterForm
 # Create your views here.
 def logout_confirm(request):
     if request.method == 'POST':
@@ -10,3 +10,18 @@ def logout_confirm(request):
 
 def logout_done(request):
     return render(request, "users/logout-done.html")
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit=False)
+            username = form.cleaned_data.get('username') 
+            form.save()
+            return redirect ('login')
+    else:
+        form = UserRegisterForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html',context)
